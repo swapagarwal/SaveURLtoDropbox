1ba73a9fbdfde1aa583bf9bdb204eebf33749971
Swapnil Agarwal <swapnilagarwal@live.com>
2015-05-23 02:54:18 +0530

    Close #1

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 $("#btn_save").click(function() {
 	Dropbox.save($('#url').val(),$('#name').val());
 });
+$("#url").on("propertychange change keyup paste input", function(){
+	var url = $('#url').val();
+	if(url.indexOf("/") != -1){
+		$('#name').val(url.substring(url.lastIndexOf("/")+1));
+	}
+	else{
+		$('#name').val(url);
+	}
+});
